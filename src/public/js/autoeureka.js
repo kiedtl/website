ad39148c60ebc3d5a3e130e6be4abb17a22bbebb
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2020-01-29 22:38:02 -0500

    remove junk

--- src/public/js/autoeureka.js
+++
@@ -1,9 +0,0 @@
-function automaticEurekaInit() {
-	var elems = document.getElementsByTagName("a")
-	for (var i = 0; i < elems.length; i++) {
-		var _ = elems[i];
-		var txt = _.getAttribute("href");
-           _.setAttribute("class", "hint--top hint--medium");
-		_.setAttribute("aria-label", txt)
-	}
-}
